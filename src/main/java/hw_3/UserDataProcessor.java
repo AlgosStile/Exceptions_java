3417cc11f034e362f1115e5a7307dcc6bb645019
AlgosStile <oleg.todor@mail.ru>
2023-06-09 15:08:58 +0300

    hw3

---
+++ src/main/java/hw_3/UserDataProcessor.java
@@ -0,0 +1,111 @@
+package hw_3;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Scanner;
+
+/**
+ * Формат ввода данных для проверки следующий:
+ * Фамилия Имя Отчество 09.06.2023 89663758587 f
+ * или так:
+ * Фамилия Имя Отчество 09.06.2023 89663758587 m
+ */
+public class UserDataProcessor {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] data = input.split(" ");
+
+        try {
+            writeUserDataToFile(data);
+            System.out.println("Данные успешно записаны в файл.");
+        } catch (IllegalArgumentException | IOException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private static void writeUserDataToFile(String[] data) throws IllegalArgumentException, IOException {
+        if (data.length != 6) {
+            throw new IllegalArgumentException("Неверное количество параметров.");
+        }
+
+        String lastName = data[0];
+        String firstName = data[1];
+        String middleName = data[2];
+        String birthDateStr = data[3];
+        String phoneNumberStr = data[4];
+        String genderStr = data[5];
+
+        LocalDate birthDate;
+        try {
+            birthDate = LocalDate.parse(birthDateStr, DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        } catch (DateTimeParseException e) {
+            throw new IllegalArgumentException("Неверный формат даты рождения.", e);
+        }
+
+        long phoneNumber;
+        try {
+            phoneNumber = Long.parseLong(phoneNumberStr);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Неверный формат номера телефона.", e);
+        }
+
+        Gender gender = Gender.fromCode(genderStr);
+        if (gender == null) {
+            throw new IllegalArgumentException("Неверный формат пола.");
+        }
+
+        String fileName = lastName + ".txt";
+        FileWriter fileWriter;
+
+        try {
+            fileWriter = new FileWriter(fileName, true);
+        } catch (IOException e) {
+            throw new IOException("Ошибка при открытии файла.", e);
+        }
+
+        try {
+            fileWriter.write(lastName + firstName + middleName + birthDate + " " + phoneNumber + gender.getCode() + "\n");
+            System.out.println("Данные успешно записаны в файл.");
+        } catch (IOException e) {
+            throw new IOException("Ошибка при записи данных в файл.", e);
+        } finally {
+            fileWriter.close();
+        }
+    }
+}
+
+enum Gender {
+    MALE('m'),
+    FEMALE('f');
+
+    private final char code;
+
+    Gender(char code) {
+        this.code = code;
+    }
+
+    public char getCode() {
+        return code;
+    }
+
+    public static Gender fromCode(String codeStr) {
+        if (codeStr == null || codeStr.isEmpty()) {
+            return null;
+        }
+
+        char code = codeStr.charAt(0);
+
+        for (Gender gender : values()) {
+            if (gender.getCode() == code) {
+                return gender;
+            }
+        }
+
+        return null;
+    }
+}
--- src/main/java/org/example/Ex_homework_2.java
+++ src/main/java/org/example/Ex_homework_2.java
@@ -1,70 +1,70 @@
-package org.example;
-
-import java.util.Scanner;
-
-public class Ex_homework_2 {
-    // Вот реализация метода для запроса дробного числа:
-    public static float getFloatInput(Scanner scanner) {
-        System.out.print("Введите дробное число: ");
-        while (!scanner.hasNextFloat()) {
-            System.out.println("Вы ввели неверный формат числа. Попробуйте еще раз.");
-            scanner.nextLine(); // читаем лишний символ перевода строки
-        }
-        return scanner.nextFloat();
-    }
-
-    // Для задания 2, нужно убедиться, что переменная num инициализируется перед использованием, например:
-    int num = 0;
-do
-
-    {
-        System.out.println("Введите положительное число: ");
-        try {
-            num = Integer.parseInt(scanner.nextLine());
-        } catch (NumberFormatException e) {
-            System.out.println("Некорректный формат числа!");
-        }
-    } while(num <=0);
-System.out.println("Вы ввели число "+num);
-
-
-   // Для задания 3, необходимо добавить оператор break после каждой кейс-ветки в блоке switch:
-            switch (dayOfWeek) {
-        case 1:
-            System.out.println("Понедельник");
-            break;
-        case 2:
-            System.out.println("Вторник");
-            break;
-        case 3:
-            System.out.println("Среда");
-            break;
-        case 4:
-            System.out.println("Четверг");
-            break;
-        case 5:
-            System.out.println("Пятница");
-            break;
-        case 6:
-            System.out.println("Суббота");
-            break;
-        case 7:
-            System.out.println("Воскресенье");
-            break;
-        default:
-            System.out.println("Такого дня недели не существует");
-            break;
-    }
-
-    //Наконец, для задания с исключением, можно реализовать следующий метод:
-    public static String getNonEmptyStringInput(Scanner scanner) throws Exception {
-        System.out.print("Введите строку: ");
-        String input = scanner.nextLine();
-        if (input.isEmpty()) {
-            throw new Exception("Строка не может быть пустой!");
-        }
-        return input;
-    }
-    //Этот метод (код выше) запрашивает у пользователя ввод строки и выбрасывает исключение, если строка пустая.
-    // Если строка не пустая, метод возвращает ее значение.
-}
+//package org.example;
+//
+//import java.util.Scanner;
+//
+//public class Ex_homework_2 {
+//    // Вот реализация метода для запроса дробного числа:
+//    public static float getFloatInput(Scanner scanner) {
+//        System.out.print("Введите дробное число: ");
+//        while (!scanner.hasNextFloat()) {
+//            System.out.println("Вы ввели неверный формат числа. Попробуйте еще раз.");
+//            scanner.nextLine(); // читаем лишний символ перевода строки
+//        }
+//        return scanner.nextFloat();
+//    }
+//
+//    // Для задания 2, нужно убедиться, что переменная num инициализируется перед использованием, например:
+//    int num = 0;
+//do
+//
+//    {
+//        System.out.println("Введите положительное число: ");
+//        try {
+//            num = Integer.parseInt(scanner.nextLine());
+//        } catch (NumberFormatException e) {
+//            System.out.println("Некорректный формат числа!");
+//        }
+//    } while(num <=0);
+//System.out.println("Вы ввели число "+num);
+//
+//
+//   // Для задания 3, необходимо добавить оператор break после каждой кейс-ветки в блоке switch:
+//            switch (dayOfWeek) {
+//        case 1:
+//            System.out.println("Понедельник");
+//            break;
+//        case 2:
+//            System.out.println("Вторник");
+//            break;
+//        case 3:
+//            System.out.println("Среда");
+//            break;
+//        case 4:
+//            System.out.println("Четверг");
+//            break;
+//        case 5:
+//            System.out.println("Пятница");
+//            break;
+//        case 6:
+//            System.out.println("Суббота");
+//            break;
+//        case 7:
+//            System.out.println("Воскресенье");
+//            break;
+//        default:
+//            System.out.println("Такого дня недели не существует");
+//            break;
+//    }
+//
+//    //Наконец, для задания с исключением, можно реализовать следующий метод:
+//    public static String getNonEmptyStringInput(Scanner scanner) throws Exception {
+//        System.out.print("Введите строку: ");
+//        String input = scanner.nextLine();
+//        if (input.isEmpty()) {
+//            throw new Exception("Строка не может быть пустой!");
+//        }
+//        return input;
+//    }
+//    //Этот метод (код выше) запрашивает у пользователя ввод строки и выбрасывает исключение, если строка пустая.
+//    // Если строка не пустая, метод возвращает ее значение.
+//}
